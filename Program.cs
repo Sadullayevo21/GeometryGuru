b5e67bf7d426a82780df62520602edccd2e21449
Sadullayev_o21 <muhammadziyosadullayev755@gmail.com>
2026-04-30 20:03:06 +0500

    Updated

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("GeometryGuru Loyihasiga Xush Kelibsiz");
+Console.WriteLine("Uchburchak peremetrini hisoblash");
+
+Console.Write("1-tomonni kiriting: ");
+decimal a = Convert.ToDecimal(Console.ReadLine());
+Console.Write("2-tomonni kiriting: ");
+decimal b = Convert.ToDecimal(Console.ReadLine());
+Console.Write("3-tomonni kiriting: ");
+decimal c = Convert.ToDecimal(Console.ReadLine()); // P = a + b + c
+decimal p = a + b + c;
+Console.WriteLine($"Uchburchak perimetri: {p}");
